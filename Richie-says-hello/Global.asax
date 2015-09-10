3cb96ffeac2eed4983f133e7acf04fb80921ba87
Richie Rowe <Richard.Rowe@Quest.com>
2015-09-10 17:39:53 +1000

    richie-says hello files

---
+++ Richie-says-hello/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Richie_says_hello.MvcApplication" Language="C#" %>
---
+++ Richie-says-hello/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Richie_says_hello.Startup))]
+namespace Richie_says_hello
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
